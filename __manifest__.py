cf16ade22d1fa472c35055092721ddbfa54a936c
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-07-25 10:59:10 +0800

    Initialisasi Modul Barokah

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Barokah Module",
+
+    'summary': """
+        Modul Tambahan untuk Report""",
+
+    'description': """
+        Form tambahan untuk report pdf dan excel untuk pt Berkah bersaudara
+    """,
+
+    'author': "Khoerul Mutaqin",
+    'website': "https://barokahperkasagroup.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','mail', 'stock', 'purchase'],
+
+    # always loaded
+    'data': [
+        'views/stock_picking.xml',
+        'security/stock_picking_inherit.xml',
+        'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ models/stock_picking_inherit.py
@@ -0,0 +1,288 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
+from datetime import datetime
+from odoo.tools import date_utils
+from datetime import timedelta
+
+
+class StockPickingInherit(models.Model):
+    _inherit = 'stock.picking'
+
+    def _get_default_received_by(self):
+        return self.env["res.users"].browse(self.env.uid)
+
+    def button_multiple_Validate(self):
+        self.action_done()
+        return
+
+    @api.depends('move_lines.quantity_done')
+    def _compute_siap_validate(self):
+        precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        for rec in self:
+            no_overquantity = rec.move_lines.filtered(
+                lambda move: move.product_uom_qty != 0 and float_compare(move.quantity_done, move.product_uom_qty,
+                                                                         precision_rounding=move.product_uom.rounding) == 1
+            )
+            no_quantities_done = all(
+                float_is_zero(move_line.qty_done, precision_digits=precision_digits) for move_line in
+                rec.move_line_ids.filtered(lambda m: m.state not in ('done', 'cancel')))
+
+            if no_overquantity or no_quantities_done:
+                rec.update({
+                    'siap_validate': False
+                })
+
+            if not no_overquantity and not no_quantities_done:
+                rec.update({
+                    'siap_validate': True
+                })
+            else:
+                rec.update({
+                    'siap_validate': False
+                })
+                
+
+    siap_validate = fields.Boolean(
+        string="Siap Validate", compute='_compute_siap_validate',readonly=True, default=False, store=True
+    )
+    
+    location_dest_id = fields.Many2one('stock.location', string='Destination Location')
+     
+    purchase_id = fields.Many2one(
+        'purchase.order',
+        string='Nomor PO'
+    )
+    
+    # siap_validate = fields.Boolean(
+    #     'siap_validate'
+    # )
+
+    second_state = fields.Selection(
+        string='Status Order',
+        selection=[('onprogress', 'On Progress'), ('transit', 'Transit Ready'), ('nottransit','Barang Stock')],
+        default="onprogress",
+        readonly=True
+
+    )
+
+    transit_ready_date = fields.Datetime(
+        string='Tanggal Ready Barang',
+        readonly=True
+    )
+
+    received_by = fields.Many2one(
+        string='Received By',
+        comodel_name='res.users',
+        ondelete='restrict',
+        default=_get_default_received_by,
+        readonly=True
+    )
+    
+
+    product_id = fields.Many2one( string='Product')
+    # product_id = fields.Many2one('product.product', 'Product', related='move_lines.product_id', readonly=True)
+
+    is_editable = fields.Boolean(
+        string="Is editable", compute="_compute_is_editable", readonly=True
+    )
+
+    def notATransit(self):
+        return self.write({
+            "second_state":'nottransit',
+            "transit_ready_date":False
+        })
+
+    def buttonTransit(self):
+        return self.write({
+            "second_state": "transit",
+            "transit_ready_date": fields.Datetime.now
+        })
+
+    @api.depends("state")
+    def _compute_is_editable(self):
+        for rec in self:
+            if rec.state in ("first_approved", "approved", "rejected", "done"):
+                rec.is_editable = False
+            else:
+                rec.is_editable = True
+
+
+class StockMoveInherit(models.Model):
+    _inherit = 'stock.move'
+
+    actual_supply = fields.Date(
+        string='Actual Supply',
+        readonly=False,
+    )
+
+    arrival_date = fields.Datetime(
+        string='Arrival Date',
+        readonly=False,
+        default=fields.Datetime.now(),
+    )
+
+    nomor_do = fields.Char(
+        string='Nomor DO',
+        required=False)
+
+    penerima_do = fields.Char(
+        string='Penerima',
+        required=False)
+
+    received_by = fields.Char(
+        string='Received By',
+    )
+
+    # received_by = fields.Many2one(
+    #     'res.users',
+    #     string='Received By',
+    #     related='picking_id.received_by',
+    #     readonly=True,
+    #     store=True,
+    # )
+
+    lokasi_id = fields.Many2one(
+        comodel_name='lokasi.lokasi',
+        string='Lokasi Supply',
+        readonly=True,
+    )
+
+    requested_by = fields.Many2one(
+        comodel_name="res.users",
+        string="Requested by",
+        readonly=True,
+    )
+
+    date_start = fields.Date(
+        string='Date Start',
+        readonly=True,
+    )
+
+    nomor_request = fields.Char(
+        string='Nomor Request',
+        readonly=True,
+    )
+
+    name_product = fields.Char(
+        string='Product Description',
+        readonly=True,
+    )
+
+    request_accepted_date = fields.Datetime(
+        string="Request Accepted Date",
+        readonly=True,
+    )
+
+    vessel_id = fields.Many2one(
+        'shipping.vessel',
+        string='Vessel',
+        readonly=True,
+    )
+
+    divisi_id = fields.Many2one(
+        'hr.department',
+        string='Divisi',
+        readonly=True,
+    )
+
+    code_budget_id = fields.Char(
+        string="Code Budget"
+    )
+
+    # code_budget_id = fields.Many2one(
+    #     comodel_name="code.budget",
+    #     string="Code Budget",
+    #     readonly=True,
+    # )
+
+    specs = fields.Text(string="Specifications", readonly=True, )
+
+    priority_request = fields.Selection(
+        string='Priority',
+        selection=[('high', 'H'), ('medium', 'M'), ('low', 'L')],
+        readonly=True,
+    )
+
+    arrival_estimation = fields.Integer(
+        string='Days',
+        readonly=True,
+    )
+
+    status_list = fields.Selection(
+        string='Status Type',
+        selection=
+        [('onprogress', 'On Progress'),
+         ('parsial', 'Parsial'),
+         ('hold', 'Hold'),
+         ('cancel', 'Cancel'),
+         ('close', 'Close')],
+        readonly=False,
+    )
+
+    keterangan_status = fields.Selection(
+        selection=[('ready', 'Ready'), ('indent', 'Indent'),
+                   ('konfirmasi', 'Konfirmasi'),
+                   ('delivery', 'Delivery'),('close', 'Close')],
+        string="Keterangan Status",
+        readonly=False,
+    )
+
+    remark = fields.Char(
+        string='Remark',
+        readonly=False,
+    )
+    
+
+    project_id = fields.Char(
+        string='Project / Unit'
+    )
+
+    # project_id = fields.Many2one(
+    #     'project.unit',
+    #     string='Project / Unit',
+    #     readonly=True,
+    # )
+
+    date_done = fields.Datetime('Actual Supply', help="Date at which the transfer has been processed or cancelled.")
+
+    # date_done = fields.Datetime('Actual Supply', copy=False, readonly=True,
+    #                             related='picking_id.date_done',
+    #                             store=True,
+    #                             help="Date at which the transfer has been processed or cancelled.")
+
+    vendor_id = fields.Many2one('res.partner', string='Vendor', readonly=True,)
+
+    code_vessel = fields.Char(
+        tring='Kode Kapal'
+    )
+
+    # code_vessel = fields.Char(
+    #     tring='Kode Kapal',
+    #     related='vessel_id.code_vessel',
+    #     readonly=True,
+    # )
+
+    department_code = fields.Char(
+        string='Kode Dep.'
+     )
+
+    # department_code = fields.Char(
+    #     tring='Kode Dep.',
+    #     related='divisi_id.department_code',
+    #     readonly=True,
+    # )
+
+    code_company = fields.Char(
+        string='Code-Cop',
+        readonly=True,)
+
+    sisa_supply = fields.Integer(
+        string='Sisa Supply',
+        default=0,
+        store=True,
+    )
+    
